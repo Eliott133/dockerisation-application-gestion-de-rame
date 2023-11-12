e1276ab05e4f988dd5f9743eb21554c84f3b2865
Eliott <mauberteliott9@gmail.com>
2023-11-12 22:49:19 +0100

    désactive du debugage

--- my-train-auto-history/api/config.py
+++ my-train-auto-history/api/config.py
@@ -3,7 +3,7 @@
 # use 0.0.0.0:5000 for a docker deployment
 SERVER_HOST = '0.0.0.0'
 SERVER_PORT = 5001
-DEBUG = True
+DEBUG = False
 # CORS Configuration
 ENABLE_CORS = True  # Enable CORS compliancy only if the front app is served by another server (mostly in dev. conf)
 
